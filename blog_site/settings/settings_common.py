0cbc81d78d1d4a5037c8069a23da23bd6aa23893
_whitecat_22 <gattinobianco22@gmail.com>
2020-11-29 20:36:02 +0900

    静的ファイル格納パスの変更

--- blog_site/settings/settings_common.py
+++ blog_site/settings/settings_common.py
@@ -130,10 +130,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'staticfiles', 'media_root')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles', 'static_root')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
