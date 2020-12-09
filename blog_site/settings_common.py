c4b28ae1377c2ad3b6f5ff6d9a9abd4d4737824f
_whitecat_22 <gattinobianco22@gmail.com>
2020-12-09 14:28:52 +0900

    settings.common修正

--- blog_site/settings_common.py
+++ blog_site/settings_common.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 import os
 
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = os.environ.get('SECRET_KEY')
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 # BASE_DIR = Path(__file__).resolve().parent.parent
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
